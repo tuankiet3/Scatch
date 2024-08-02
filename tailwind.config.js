116c20f540b39e9d7ce21ac5b3555324dbd95e48
tuankiet3 <vohtuankiet@dtu.edu.vn>
2024-08-02 16:22:58 +0700

    base project

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+    content: ["./**/*.html"], // Đường dẫn đến các tệp HTML của bạn
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
+
+<script src="https://cdn.tailwindcss.com"></script>;
